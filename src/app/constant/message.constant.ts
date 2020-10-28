5c9e2c58c00c047b33da55b900ecd5c432bb1290
ryodocoder1997 <ryodocoder1997@gmail.com>
2020-10-29 02:35:37 +0700

    29/10/2020 - 2:35 A.M - Add Login and Logout Feature

---
+++ src/app/constant/message.constant.ts
@@ -0,0 +1,24 @@
+export const messageTitle = {
+  SUCCESS: "Thành Công",
+  FAILED: "Thất Bại",
+  WARNING: "Cảnh Báo",
+};
+
+export const messageContent = {
+  LOGIN_SUCCESS: "Đăng nhập thành công !!!",
+  LOGIN_FAILED:
+    "Đăng nhập thất bại, vui lòng kiểm tra lại tài khoản hoặc mật khẩu!",
+  LOG_OUT_SUCCESS: "Đăng xuất thành công !!!",
+  GET_USER_INFO_FAILED:
+    "Lấy thông tin đăng nhập thất bại, vui lòng đăng nhập lại",
+  WELCOME_USER: "Xin chào, ",
+  WELCOME_DEFAULT: "Quản lý nhà hàng",
+};
+
+export const dialogMessage = {
+  LOG_OUT_MESSAGE: "Bạn thật sự muốn đăng xuất?",
+};
+
+export const dialogTitle = {
+  LOG_OUT_TITLE: "Đăng xuất",
+};
---
+++ src/app/models/user.model.ts
@@ -0,0 +1,15 @@
+export interface User {
+    employeeCode: string,
+    identityCardCode: string,
+    name: string,
+    role: UserRole,
+    username: string
+
+    
+}
+
+export enum UserRole{
+    Manager = 0,
+    Chief = 1,
+    Waiter = 2
+}
